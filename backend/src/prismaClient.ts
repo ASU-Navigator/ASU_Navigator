959a3a45606fd39c7315545e6b8556c5e09052ec
Xander <87785817+XanderYZZ@users.noreply.github.com>
2026-04-01 16:56:19 -0700

    Set pg pool max=1 for serverless

--- backend/src/prismaClient.ts
+++ backend/src/prismaClient.ts
@@ -3,7 +3,7 @@ import { PrismaClient } from "@prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
 import { Pool } from "pg";
 
-const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+const pool = new Pool({ connectionString: process.env.DATABASE_URL, max: 1 });
 const adapter = new PrismaPg(pool);
 
 export const prisma = new PrismaClient(
